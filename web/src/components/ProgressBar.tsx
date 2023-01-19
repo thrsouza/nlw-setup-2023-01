bf335a19b3bc951ed0a19d69f4eac9e33561870d
Thiago Souza <dev@thiagosouza.com>
2023-01-18 21:15:59 -0300

    chore: web updates class-3

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,21 @@
+import * as Progress from "@radix-ui/react-progress";
+
+interface ProgressBarProps {
+  progress: number;
+}
+
+export function ProgressBar({ progress }: ProgressBarProps) {
+  const progressIndicatorStyles = {
+    width: `${progress}%`,
+  };
+
+  return (
+    <Progress.Root className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <Progress.Indicator
+        aria-valuenow={progress}
+        className="h-3 rounded-xl bg-violet-600"
+        style={progressIndicatorStyles}
+      />
+    </Progress.Root>
+  );
+}
